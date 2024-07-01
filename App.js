36229fc237e7b1f15fd2f5511bdb85bc72fc00fa
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-07-02 01:29:03 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import {  createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import OnboardingScreen from './screens/OnboardingScreen'
 import LoginScreen from './screens/LoginScreen'
 
 const AppStack = createNativeStackNavigator()
 
-const App=()=>{
-  return(
+const App = () => {
+  const [isFirst, setIsFirst] = useState(null)
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true')
+        setIsFirst(true)
+      }
+      else {
+        setIsFirst(false)
+      }
+    })
+  }, [])
+
+  if (isFirst === null) {
+    return null
+  }
+  else if (isFirst === true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator
+          headerMode="none"
+        >
+          <AppStack.Screen options={{ headerShown: false }} name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    )
+  }
+  else{
+    return <LoginScreen/>
+  }
+
+  return (
     <NavigationContainer>
       <AppStack.Navigator
-      headerMode="none"
+        headerMode="none"
       >
-        <AppStack.Screen  options={{headerShown:false}} name="Onboarding" component={OnboardingScreen}/>
-        <AppStack.Screen name="Login" component={LoginScreen}/>
+        <AppStack.Screen options={{ headerShown: false }} name="Onboarding" component={OnboardingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
       </AppStack.Navigator>
     </NavigationContainer>
   )
