d259de92befb7095fa7e3f35dc9533b4eac2af43
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-07-02 01:27:21 +0530

    Add files via upload

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {View, Text, Button, StyleSheet, Alert} from 'react-native'
+import {View, Text, Button, StyleSheet, Alert, TouchableOpacity} from 'react-native'
 
 const LoginScreen=()=>{
     return(
         <View style={styles.container} >
             <Text>Login Screen</Text>
-            <Button title='Click here'
-             onPress={()=> Alert('Button Clicked')} />
+            <TouchableOpacity>
+                <Text style={{color:'blue'}}>Click Here</Text>
+            </TouchableOpacity>
         </View>
     )
 } 
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,26 +1,50 @@
-import { View, Text, StyleSheet, Button, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Onboarding from 'react-native-onboarding-swiper'
 import { LogBox } from 'react-native';
 
+
+// const Next=({...props})=>(
+//     <Button 
+//     title='Next'
+//     {...props}
+//      />
+// )
+
+const Done = ({ ...props }) => (
+    <TouchableOpacity {...props}>
+        <Text style={{ padding: 18, fontSize: 17, color: 'black' }}>Done</Text>
+        
+    </TouchableOpacity>
+)
 const OnboardingScreen = ({ navigation }) => {
     LogBox.ignoreAllLogs();
     return (
         <Onboarding
+            DoneButtonComponent={Done}
+            // NextButtonComponent={Next}
+            onSkip={() => navigation.navigate("Login")}
+            onDone={() => navigation.navigate("Login")}
             pages={[
                 {
                     backgroundColor: '#a6e4d0',
                     image: <Image source={require('../assets/onboarding-img1.png')} />,
-                    title: 'Onboarding',
-                    subtitle: 'Done with React Native Onboarding Swiper',
+                    title: 'Connect with the world',
+                    subtitle: 'A new way to connect',
                 },
                 {
-                    backgroundColor: '#FF9D00',
+                    backgroundColor: '#fdeb93',
                     image: <Image source={require('../assets/onboarding-img2.png')} />,
-                    title: 'Onboarding 2',
-                    subtitle: 'Done with React Native Onboarding Swiper',
+                    title: 'Share your thoughts',
+                    subtitle: 'Connect with similar kind of people',
                 },
-  ]}
+                {
+                    backgroundColor: '#e9bcbe',
+                    image: <Image source={require('../assets/onboarding-img3.png')} />,
+                    title: 'Become a star',
+                    subtitle: 'Let the spotlight capture you',
+                }
+            ]}
         />
     )
 }
