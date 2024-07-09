876f8d993804c8dc5b2624f0f642fd0bbb01b416
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-07-09 23:44:00 +0530

    Add files via upload

--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const AddPostScreen=()=> {
   return (
-    <View style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-      <Text style={{color:'black'}}>AddPostScreen</Text>
+    <View style={styles.container}>
+      <Text>AddPostScreen</Text>
     </View>
   )
 }
 
 export default AddPostScreen
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+
+})
