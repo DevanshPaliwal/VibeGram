2100bdb07fcaf212928f30a3808fd2b241f2626d
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-07-10 23:19:50 +0530

    Add files via upload

---
+++ styles/AddPostStyles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const InputWrapper = styled.View`
+    flex:1;
+    justify-content:center;
+    align-items:center;
+    width:100%;
+    background-color:#2e64e515;
+`
+
+export const InputField = styled.TextInput`
+    justify-content:center;
+    align-items:center;
+    font-size:25px;
+    text-align:center;
+    width:90%;
+    color:#5C5E5F;
+`
