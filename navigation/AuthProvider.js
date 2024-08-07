0ef0842319918e2e85e303ace6c88c54adf288e7
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-08-07 17:40:36 +0530

    Add files via upload

--- navigation/AuthProvider.js
+++ navigation/AuthProvider.js
@@ -1,13 +1,16 @@
 import React, { createContext, useState } from 'react'
 import auth, { firebase } from '@react-native-firebase/auth'
 import { GoogleSignin } from '@react-native-community/google-signin'
+import firestore from '@react-native-firebase/firestore'
 
 
 
 export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
+
     const [user, setUser] = useState(null)
+
     return (
         <AuthContext.Provider
             value={{
@@ -16,9 +19,58 @@ export const AuthProvider = ({ children }) => {
                 register: async (email, password) => {
                     try {
                         await auth().createUserWithEmailAndPassword(email, password)
+                            .then(() => {
+                                firestore().collection('users').doc(auth().currentUser.uid)
+                                    .set({
+                                        fname: '',
+                                        lname: '',
+                                        email: email,
+                                        createdAt: firestore.Timestamp.fromDate(new Date()),
+                                        userImg: null,
+                                    })
+                                    .catch(error => {
+                                        console.log("something went wrong with added user: ", error)
+                                    })
+                            })
+                            .catch(error => {
+                                console.log("Something went wrong with signup: ", error)
+                            })
                     }
                     catch (e) {
                         console.log(e)
+                    }
+                },
+                googleSignup: async () => {
+                    try {
+                        const { idToken } = await GoogleSignin.signIn()
+
+                        const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+
+                        await auth().signInWithCredential(googleCredential)
+
+                            .then(() => {
+                                //Once the user creation has happened successfully, we can add the currentUser into firestore
+                                //with the appropriate details.
+                                // console.log('current User', auth().currentUser);
+                                firestore().collection('users').doc(auth().currentUser.uid)
+                                    .set({
+                                        fname: '',
+                                        lname: '',
+                                        email: auth().currentUser.email,
+                                        createdAt: firestore.Timestamp.fromDate(new Date()),
+                                        userImg: null,
+                                    })
+                                    //ensure we catch any errors at this stage to advise us if something does go wrong
+                                    .catch(error => {
+                                        console.log('Something went wrong with added user to firestore: ', error);
+                                    })
+                            })
+
+                            .catch(error => {
+                                console.log('Something went wrong with sign up: ', error);
+                            });
+                    } catch (error) {
+                        console.log({ error });
                     }
                 },
                 login: async (email, password) => {
@@ -39,7 +91,7 @@ export const AuthProvider = ({ children }) => {
                         // Sign-in the user with the credential
                         await auth().signInWithCredential(googleCredential);
                     }
-                    catch(e){
+                    catch (e) {
                         console.log(e)
                     }
                 },
